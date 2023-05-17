2dd764dd8bde539c501027af96b7965cbacc8363
sa_nek <alexandrtokaychuk@gmail.com>
2023-05-17 18:10:35 +0300

    disable tools

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { store } from "./store/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
 import persistReducer from "redux-persist/es/persistReducer";
 import storage from "redux-persist/lib/storage";
-import {
-  FLUSH,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  REHYDRATE,
-} from "redux-persist";
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, appReducer);
@@ -20,4 +12,5 @@ export const store = configureStore({
     getDefaultMiddleware({
       serializableCheck: false,
     }),
+  devTools: false,
 });
